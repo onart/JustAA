2605357791fb668c52f286d5dfa9701ef59b66f1
onart <onart97@naver.com>
2020-06-29 13:31:05 +0900

    애니메이션에서 함수(이벤트)

---
+++ Assets/Scripts/Managers/AudPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudPlayer : MonoBehaviour  //애니메이터와 연계됨. 변수를 변경하는 즉시 오디오 소스를 1회 플레이
+{
+    AudioSource[] as_s;
+
+    void Start()
+    {
+        as_s = GetComponents<AudioSource>();
+    }
+
+    public void PlayOne(int idx)
+    {
+        as_s[idx].Play();
+    }
+
+}

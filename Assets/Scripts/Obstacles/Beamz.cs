1b25fc50d59aecb20fd754fb913e6d7e42026238
onart <onart97@naver.com>
2020-08-19 12:07:04 +0900

    레이저빔

--- Assets/Scripts/Obstacles/Beamz.cs
+++ Assets/Scripts/Obstacles/Beamz.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Beamz : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int mask = 0;
+
+    public GameObject laser;
+    public bool isRotating;     //제작자가 미리 정해두기. 회전 시 Update()에 angle2vector가 들어감
+    RaycastHit2D destination;
+    Vector2 dir;
+
     void Start()
     {
-        
+        angle2vector();
+        mask += (1 << LayerMask.NameToLayer("Map"));
+        mask += (1 << LayerMask.NameToLayer("Player"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isRotating) angle2vector();
+        RenderRay();
+    }
+
+    void RenderRay()
+    {
+        destination = Physics2D.Raycast(transform.position, dir, float.PositiveInfinity, mask);
+        laser.transform.localScale = new Vector2(destination.distance / 3, laser.transform.localScale.y);
+    }
+
+    void angle2vector()
+    {
+        float ang = transform.eulerAngles.z - 90;
+        laser.transform.eulerAngles = new Vector3(0, 0, ang);
+        ang *= Mathf.Deg2Rad;
+        dir = new Vector2(Mathf.Cos(ang), Mathf.Sin(ang));
     }
 }

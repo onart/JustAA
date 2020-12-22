832d3ff06b189f4470971ef88c34f69e899e9523
onart <onart97@naver.com>
2020-12-22 14:41:26 +0900

    메뉴 페이지

---
+++ Assets/Scripts/UIRelated/MenuPager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    enum State { CTRL, TOGGLE, };
+    public GameObject[] pages;
+    State state;
+
+    void Start()
+    {
+        state = State.CTRL;
+    }
+
+    public void setPage(int i)
+    {
+        pages[(int)state].SetActive(false);
+        state = (State)i;
+        pages[(int)state].SetActive(true);
+    }
+}

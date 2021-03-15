bf9140bddec755f61fb2bc72f4b226942fb4ccae
onart <onart97@naver.com>
2021-03-15 21:10:24 +0900

    Options에서 일부 값을 제외할 수 있도록 수정

--- Assets/Scripts/Managers/TalkManager.cs
+++ Assets/Scripts/Managers/TalkManager.cs
@@ -1,17 +1,19 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TalkManager : MonoBehaviour
 {
     private int state, idx, wait;  //대화 진행 상태. 디폴트 -1 상태에서는 대화상자 OFF 상태. wait는 오류수정용 1프레임 대기. wait=2인 경우 선택지가 켜져 있음
-    public int selection, opcount;
+    public int selection, cursor;
     public TextMeshProUGUI notice, arrow;
 
     Entity ent; //선택지 답을 받을 개체. 이벤트는 개체별로 존재
     TextMeshProUGUI options;
     Image optBox;
     DialogBox dBox;
+    Options op;
     public GameObject dBoxUI, optionUI;
 
     private void Start()
@@ -26,7 +28,6 @@ public class TalkManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I)) print(Entity.cooltime);
         if (!SysManager.menuon)
         {
             if (wait == 0 && Input.GetKeyDown(SysManager.keymap["상호작용"]) && state > -1) //대화 넘기기 페이즈
@@ -38,11 +39,13 @@ public class TalkManager : MonoBehaviour
             {
                 if (Input.GetKeyDown(KeyCode.UpArrow))
                 {
-                    if (--selection < 0) selection = opcount - 1;
+                    selection = op.up1(selection);
+                    print(selection);
                 }
                 if (Input.GetKeyDown(KeyCode.DownArrow))
                 {
-                    if (++selection > opcount - 1) selection = 0;
+                    selection = op.down1(selection);
+                    print(selection);
                 }
                 RenderArrow();
                 if (Input.GetKeyDown(SysManager.keymap["상호작용"]))
@@ -55,8 +58,10 @@ public class TalkManager : MonoBehaviour
 
     void RenderArrow()
     {
+        int itr = 0;
         arrow.text = "";
-        for (int i = 0; i < opcount; i++)
+        int num = op.Number;
+        for (int i = 0; itr < num; i = op.down1(i), itr++)
         {
             if (i == selection) arrow.text += " >";
             else arrow.text += "\n";
@@ -64,7 +69,7 @@ public class TalkManager : MonoBehaviour
         arrow.text += ' ';
     }
 
-    void SelectON(Options op)
+    void SelectON()
     {
         if (op.Number == 0)
         {
@@ -73,7 +78,6 @@ public class TalkManager : MonoBehaviour
         }
         Vector2 v2;
         wait = 2;
-        opcount = op.Number;
         optionUI.SetActive(true);
         optBox.rectTransform.localScale = new Vector2(op.Hor, op.Number);
         options.text = op.Body;
@@ -105,7 +109,7 @@ public class TalkManager : MonoBehaviour
         if (ent) ent.afterDialog();
         ent = null;
     }
-    public void Dialog_Start(int index, Entity en)
+    public void Dialog_Start(int index, Entity en, List<int> exc = null)
     {
         if (state == -1 && Entity.cooltime == 0)
         {
@@ -114,6 +118,8 @@ public class TalkManager : MonoBehaviour
             en.dialog = index;
             idx = index;
             wait = 1;
+            op = BaseSet.ops[BaseSet.talkData[idx].Item2].Clone();
+            if (exc != null) op.delOps(exc);
             Dialog_Go();
         }
     }
@@ -126,7 +132,7 @@ public class TalkManager : MonoBehaviour
             var temp2 = temp1.Item1[state];
             if (temp2.Item1 == BaseSet.Chars.END)
             {
-                SelectON(BaseSet.ops[temp1.Item2]);
+                SelectON();
                 return;
             }
             dBox.InitBox(temp2.Item1, temp2.Item2, temp2.Item3);
--- Assets/Scripts/UIRelated/Options.cs
+++ Assets/Scripts/UIRelated/Options.cs
@@ -1,23 +1,80 @@
-﻿public class Options
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Options
 {
     int number;         //선택지 수(세로로 선택상자를 늘임)
+    List<int> exc;          //제외할 인덱스
     string body;
-    float horizontal;   //가로로 선택상자를 늘임)
+    float horizontal;   //(가로로 선택상자를 늘임)
 
     public Options(int num, string str, float hr)
     {
         number = num;
         body = str;
         horizontal = hr;
+        exc = new List<int>();
+    }
+
+    public void delOps(List<int> idx)
+    {
+        exc = new List<int>(idx);
+    }
+
+    public int down1(int cur)
+    {
+        while (exc.Contains(++cur)) ;
+        if (cur >= number)
+        {
+            return down1(-1);
+        }
+        else
+        {
+            return cur;
+        }
+    }
+
+    public int up1(int cur)
+    {
+        while (exc.Contains(--cur)) ;
+        if (cur < 0)
+        {
+            return up1(number);
+        }
+        else
+        {
+            return cur;
+        }
+    }
+
+    public Options Clone()
+    {
+        return (Options)MemberwiseClone();
     }
 
     public int Number
     {
-        get { return number; }
+        get
+        {
+            return number - exc.Count();
+        }
     }
     public string Body
     {
-        get { return body; }
+        get
+        {
+            string ret = "";
+            var b = body.Split('\n');
+            for (int i = 0; i < number; i++)
+            {
+                if (!exc.Contains(i))
+                {
+                    if (ret.Length == 0) ret += b[i];
+                    else ret += ('\n' + b[i]);
+                }
+            }
+            return ret;
+        }
     }
     public float Hor
     {
